dbd81da287824581442387ed6ace452eafd8df90
Sungeheon <elon4856@gmail.com>
2021-11-18 19:03:58 +0900

    add: dto 추가.

---
+++ dto.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing import List
+class User:
+    use_deer_name: str
+    use_end_lat: float
+    use_end_lng: float
+
+    use_start_at: datetime
+    use_end_at: datetime
+
+
+class Deer:
+    deer_name: str
+    deer_area_id: int
+
+class Area:
+    area_id: int
+    area_bounday: List[tuple]
+    #실제는 폴리곤타입이라고 함.
+    area_center: tuple
+    area_coords: List[tuple]
+
+class ParkingZone:
+    parkingzone_id: int
+    parkingzone_center_lat: float
+    parkingzone_center_lng: float
+    parkingzone_radius: float
+
+
+class ForbiddenArea:
+    forbidden_area_id: int
+    forbidden_area_boundary: List[tuple]
+    forbidden_area_coords: List[tuple]
