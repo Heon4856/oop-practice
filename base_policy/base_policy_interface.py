320ddff1ed783e6b8b80cc4df59d22796caf5d97
Sungeheon <elon4856@gmail.com>
2021-11-19 16:57:03 +0900

    fix: 잘못된 변수설정 변경.

--- base_policy/base_policy_interface.py
+++ base_policy/base_policy_interface.py
@@ -2,21 +2,23 @@ from abc import ABC, abstractmethod
 
 
 class BasePricing(ABC):
-    @property
-    def basic_rate(self):
-        return f"basic_rate = {self._basic_rate}"
 
-    @basic_rate.setter
+
+    @property
     def basic_rate(self):
         return self._basic_rate
 
+    @basic_rate.setter
+    def basic_rate(self, new_basic_rate: int):
+        self._basic_rate = new_basic_rate
+
     @property
     def per_minute_rate(self):
-        return f"per_minute_rate = {self._per_minute_rate}"
+        return self._per_minute_rate
 
     @per_minute_rate.setter
-    def per_minute_rate(self):
-        return self._per_minute_rate
+    def per_minute_rate(self, new_per_minute_rate: int):
+        self._per_minute_rate =new_per_minute_rate
 
 
     @abstractmethod
