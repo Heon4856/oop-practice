26d05fe5256381d1e6f91f532627c4a13c9a9bed
Sungeheon <elon4856@gmail.com>
2021-11-25 01:26:21 +0900

    django-version

--- main.py
+++
@@ -1,8 +0,0 @@
-from cost_calculator import CostCalculator
-from dto import User
-## api
-
-# @app.route()
-# def cost_reply:
-#     cost_calculator = CostCalculator(user: User)
-#     return cost_calculator.calculate()
